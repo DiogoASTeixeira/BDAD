9e2fef061e089b8da2ab1c0f794d2fe272ecd791
DiogoASTeixeira <36501821+DiogoASTeixeira@users.noreply.github.com>
2019-04-09 15:54:38 +0100

    Add files via upload

--- criar.sql
+++ criar.sql
@@ -1,35 +1,115 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS studio;
-DROP TABLE IF EXISTS game;
-DROP TABLE IF EXISTS achievement;
-DROP TABLE IF EXISTS users_achievement;
-DROP TABLE IF EXISTS review;
-DROP TABLE IF EXISTS ownership;
-DROP TABLE IF EXISTS wishlist;
-DROP TABLE IF EXISTS follow_studio;
-DROP TABLE IF EXISTS friendship;
-DROP TABLE IF EXISTS communities;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS privileges;
 DROP TABLE IF EXISTS comment;
-DROP TABLE IF EXISTS priviliges;
+DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS communities;
+DROP TABLE IF EXISTS friendship;
+DROP TABLE IF EXISTS follow_studio;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS ownership;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS user_achievement;
+DROP TABLE IF EXISTS achievement;
+DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS studio;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    email VARCHAR NOT NULL UNIQUE,
+    name VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL UNIQUE,
     wallet REAL DEFAULT 0.0,
-    country VARCHAR
+    country VARCHAR(64),
+	CHECK (wallet >= 0.0)
 );
 
 CREATE TABLE studio (
     id INTEGER PRIMARY KEY,
-    name VARCHAR NOT NULL UNIQUE,
+    name VARCHAR(64) NOT NULL UNIQUE,
     description TEXT
 );
 
 CREATE TABLE game (
     id INTEGER PRIMARY KEY,
-    title VARCHAR NOT NULL,
+    title VARCHAR(128) NOT NULL,
     price REAL NOT NULL,
-    rating REAL CHECK(rating >= 0 AND rating =< 100)
+    rating REAL CHECK(rating >= 0 AND rating <= 100),
+    developer_id INTEGER REFERENCES studio,
+	publisher_id INTEGER REFERENCES studio
 );
+
+CREATE TABLE achievement (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    description TEXT NOT NULL,
+    game_id INTEGER REFERENCES game
+);
+
+/* CHECK KEYS */
+CREATE TABLE user_achievement (
+	user_id INTEGER REFERENCES users,
+    achievement_id INTEGER REFERENCES achievement,
+    CONSTRAINT pk_achiev PRIMARY KEY (user_id, achievement_id)
+);
+
+CREATE TABLE review (
+	user_id INTEGER REFERENCES users,
+    game_id INTEGER REFERENCES game,
+    body TEXT,
+    rating REAL NOT NULL,
+    CONSTRAINT pk_review PRIMARY KEY (user_id, game_id)
+);
+
+CREATE TABLE ownership (
+	user_id INTEGER REFERENCES users,
+    game_id INTEGER REFERENCES game,
+    CONSTRAINT pk_own PRIMARY KEY (user_id, game_id)
+);
+
+CREATE TABLE wishlist (
+	user_id INTEGER  REFERENCES users,
+    game_id INTEGER  REFERENCES game,
+    CONSTRAINT pk_wish PRIMARY KEY (user_id, game_id)
+);
+
+CREATE TABLE follow_studio (
+	user_id INTEGER  REFERENCES users,
+    studio_id INTEGER  REFERENCES studio,
+    CONSTRAINT pk_follow PRIMARY KEY (user_id, studio_id)
+);
+
+CREATE TABLE friendship(
+	user1 INTEGER  REFERENCES users,
+    user2 INTEGER  REFERENCES users,
+    CHECK (user1 > user2),
+    CONSTRAINT pk_friend PRIMARY KEY (user1, user2)
+);
+
+CREATE TABLE communities (
+	id INTEGER PRIMARY KEY,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE post (
+	id INTEGER PRIMARY KEY,
+    title VARCHAR(32) NOT NULL,
+    body TEXT NOT NULL,
+    rating REAL DEFAULT 50,
+    author_id INTEGER  REFERENCES users,
+    community_id INTEGER  REFERENCES communities
+);
+
+CREATE TABLE comment (
+	id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    rating REAL DEFAULT 50,
+    author_id INTEGER  REFERENCES users,
+    post_id INTEGER  REFERENCES post
+);
+
+CREATE TABLE privileges (
+    user_id INTEGER  REFERENCES users,
+    community_id INTEGER  REFERENCES communities,
+    is_administrator BOOLEAN DEFAULT false,
+    CONSTRAINT pk_privilege PRIMARY KEY (user_id, community_id)
+);
+
---
+++ povoar.sql
@@ -0,0 +1,17 @@
+INSERT INTO users VALUES (1, 'donaldthefirst', 'dtf@trump.com', 9000.00, 'USA');
+INSERT INTO users VALUES (2, 'SUPERGERALTOBROS', 'geralto@rivia.com', 2.00, 'Novigrad');
+INSERT INTO users VALUES (3, 'HuninoSayoko', 'weeb@owo.com', 1000.00, 'Japan');
+INSERT INTO users VALUES (4, 'LastLombax', 'ratchet@clank.com', 50.00, 'Zona Lombar');
+INSERT INTO users VALUES (5, 'ShootyMcFace', 'shoot@face.com', 0.00, 'Pandora');
+INSERT INTO users VALUES (6, 'tinyTINA', 'cookie_conossieur@badonkadonks.com', 13.00, 'Pandora');
+
+INSERT INTO studio VALUES (1, '2K', '2K develops and publishes critically-acclaimed franchises such as BioShock, Borderlands, Sid Meier’s Civilization, XCOM, WWE 2K, and NBA 2K.');
+INSERT INTO studio VALUES (2, 'GearBox', 'Growing developer studio famous for the Borderlands Universe');
+INSERT INTO studio VALUES (3, 'CD PROJEKT RED', 'CD PROJEKT RED is a development studio founded in 2002. Our mission is to tell emotional stories riddled with meaningful choices and consequences, as well as featuring characters gamers can truly connect with.');
+INSERT INTO studio VALUES (4, 'Telltale Games', 'We make story-driven games based on The Walking Dead, Guardians of the Galaxy, Minecraft, Batman, and more!');
+
+INSERT INTO game VALUES (1, 'Borderlands', 20.0, 93.0, 2, 1);
+INSERT INTO game VALUES (2, 'The Witcher 3', 30.0, 95.0, 3, 3);
+INSERT INTO game VALUES (3, 'Bioshock', 22.2, 89.3, 1, 1);
+INSERT INTO game VALUES (4, 'Tales From The Borderlands', 18.5, 73.5, 4, 1);
+
