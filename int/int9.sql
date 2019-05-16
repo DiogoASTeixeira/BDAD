19e65c04d2a992abeb38ca9f90b4ff0a1b8d864f
Meirinha <up201604350@fe.up.pt>
2019-05-16 11:28:16 +0100

    ownsership was badly writen

--- int/int9.sql
+++ int/int9.sql
@@ -5,4 +5,4 @@ SELECT ... FROM
 ((SELECT F1.user1 FROM friendship F1 WHERE F1.user2 = :user_id) UNION
 (SELECT F2.user2 FROM friendship F2 WHERE F2.user1 = :user_id))
 
-JOIN ownsership on ownsership.users = achievement.id)
+JOIN ownership on ownership.users = achievement.id)
