3abd73e3399ce9c6b8a7c1a2bb8aba6bcc6bb9e1
Meirinha <up201604350@fe.up.pt>
2019-05-18 14:12:15 +0100

    new query on int9

--- int/int9.sql
+++ int/int9.sql
@@ -1,8 +1,19 @@
 /**
 * Games that at least one friend has and the user doesn't
 */
-SELECT ... FROM
-((SELECT F1.user1 FROM friendship F1 WHERE F1.user2 = :user_id) UNION
-(SELECT F2.user2 FROM friendship F2 WHERE F2.user1 = :user_id))
 
-JOIN ownership on ownership.users = achievement.id)
+/**
+* jogos dos amigos
+*/
+SELECT * FROM
+(((SELECT F1.user1 FROM friendship F1 WHERE F1.user2 = :user_id) UNION
+(SELECT F2.user2 FROM friendship F2 WHERE F2.user1 = :user_id)) JOIN (ownership on ownership.users = user1 OR ownership on ownership.users = user2))
+
+
+/*
+* jogos em comum
+*/
+SELECT * FROM
+(((SELECT F1.user1 FROM friendship F1 WHERE F1.user2 = :user_id) UNION
+(SELECT F2.user2 FROM friendship F2 WHERE F2.user1 = :user_id)) 
+JOIN (ownership on ownership.users = user1 OR ownership on ownership.users = user2) AND (SELECT game FROM ownership WHERE users = :user_id))
